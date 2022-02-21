4bb5a3fd7dca99065d7dea02d84f457b9171bc45
Jacopin Eliott <eliott.jacopin@laposte.net>
2022-02-21 14:58:51 +0100

    added libSBML
    can now include headers from libSBML to write and read SBML files

---
+++ ECell_Engine/headers/generate_models.hpp
@@ -0,0 +1,7 @@
+
+#include <sbml/SBMLTypes.h>
+
+
+SBMLDocument* createExampleEnzymaticReaction();
+//bool validateExampleSBML(SBMLDocument* sbmlDoc);
+//bool writeExampleSBML(const SBMLDocument* sbmlDoc, const string& filename);
---
+++ ECell_Engine/src/generate_models.cpp
@@ -0,0 +1,2 @@
+#include "generate_models.hpp"
+
