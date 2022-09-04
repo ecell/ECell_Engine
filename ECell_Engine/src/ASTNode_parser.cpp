e0c486728784f7758e40f64fca6d8f932e2fba60
Jacopin Eliott <eliott.jacopin@laposte.net>
2022-09-05 07:30:02 +0900

    [Fix] Account for occurence of POWER mathml node
    Major:
    - Some mathematical functions in the kinetic laws were using the POWER node (x^y) which was then deciphered as AST_FUNCTION_POWER by libsbml.
    - AST_FUNCTION_POWER was therefore added to the Evaluate functions as well as debug functions (pretty prints)
    [Fix] Account for occurence of POWER mathml node
    
    - Add include to cmath to access std::pow()

--- ECell_Engine/src/ASTNode_parser.cpp
+++ ECell_Engine/src/ASTNode_parser.cpp
@@ -200,6 +200,9 @@ float ASTEvaluator::EvaluateNode(ASTNodeEx* _node)
 	case ASTNodeType_t::AST_DIVIDE:
 		return EvaluateNode(getNode(_node->getLeftChildEx())) / EvaluateNode(getNode(_node->getRightChildEx()));
 		break;
+	case ASTNodeType_t::AST_FUNCTION_POWER:
+		return std::pow(EvaluateNode(getNode(_node->getLeftChildEx())), EvaluateNode(getNode(_node->getRightChildEx())));
+		break;
 	default:
 		return 0;
 	}
@@ -382,6 +385,9 @@ std::string ASTEvaluator::PrettyPrintFormula(ASTNodeEx* _node)
 	case ASTNodeType_t::AST_DIVIDE:
 		return PrettyPrintFormula(getNode(_node->getLeftChildEx())) + "/" + PrettyPrintFormula(getNode(_node->getRightChildEx()));
 		break;
+	case ASTNodeType_t::AST_FUNCTION_POWER:
+		return PrettyPrintFormula(getNode(_node->getLeftChildEx())) + "^" + PrettyPrintFormula(getNode(_node->getRightChildEx()));
+		break;
 	default:
 		return "UNKNOWN";
 	}
@@ -447,6 +453,9 @@ void ASTEvaluator::PrettyPrintTree(ASTNodeEx* _node, int _lvl)
 		break;
 	case ASTNodeType_t::AST_DIVIDE:
 		std::cout << std::string(_lvl, '  ') << "|-AST_DIVIDE (/" << ", " << _node->getNumChildrenEx() << " children)" << std::endl;
+		break;
+	case ASTNodeType_t::AST_FUNCTION_POWER:
+		std::cout << std::string(_lvl, '  ') << "|-AST_FUNCTION_POWER (^" << ", " << _node->getNumChildrenEx() << " children)" << std::endl;
 		break;
 	default:
 		std::cout << std::string(_lvl, '  ') << "Child is of type " << _node->getTypeEx() << std::endl;
