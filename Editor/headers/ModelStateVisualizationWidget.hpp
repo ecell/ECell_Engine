6908879f0fcfbb63c182dfdecd2372434f4c4830
Eliott <eliott.jacopin@laposte.net>
2022-09-13 20:03:32 +0900

    [Update] Complete redesign of ModelVisualizationWidget
    Major:
    - Can now add tabs dynamically with different visualization
    - Added parameters to control axis limits of LinePlot
    - Can decide to plot time or species on X or Y axis.

--- Editor/headers/ModelStateVisualizationWidget.hpp
+++ Editor/headers/ModelStateVisualizationWidget.hpp
@@ -13,28 +13,245 @@
 
 namespace ECellEngine::Editor
 {
+    // utility structure for realtime plot
+    struct RollingBuffer
+    {
+        float Span;
+        ImVector<ImVec2> Data;
+        RollingBuffer()
+        {
+            Span = 10.0f;
+            Data.reserve(2000);
+        }
+        void AddPoint(float x, float y)
+        {
+            float xmod = fmodf(x, Span);
+            if (!Data.empty() && xmod < Data.back().x)
+                Data.shrink(0);
+            Data.push_back(ImVec2(xmod, y));
+        }
+    };
+
+    // utility structure for realtime plot
+    struct ScrollingBuffer
+    {
+        int MaxSize;
+        int Offset;
+        ImVector<ImVec2> Data;
+        ScrollingBuffer(int max_size = 2000)
+        {
+            MaxSize = max_size;
+            Offset = 0;
+            Data.reserve(MaxSize);
+        }
+        void AddPoint(float x, float y)
+        {
+            if (Data.size() < MaxSize)
+                Data.push_back(ImVec2(x, y));
+            else
+            {
+                Data[Offset] = ImVec2(x, y);
+                Offset = (Offset + 1) % MaxSize;
+            }
+        }
+        void Erase()
+        {
+            if (Data.size() > 0)
+            {
+                Data.shrink(0);
+                Offset = 0;
+            }
+        }
+        void Reset()
+        {
+            Data.shrink(0);
+            Offset = 0;
+            Data.reserve(MaxSize);
+        }
+    };
+
+    struct TabData
+    {
+    private :
+        short id;
+    public:
+        char name[32];
+
+        TabData(short _id) : id(_id)
+        {
+            sprintf(name, "Tab %d", _id);
+        }
+    };
+
+    struct LinePlotAxisGraphicsData
+    {
+    public:
+        float low, up;//Corresponding limit of the axis.
+        float lowTolerance, upTolerance;//In [0,1]. Tells how much we rescale realtively to the value on the axis.
+        float axisRescaleLowThreshold, axisRescaleUpThreshold;//In [0,1]. Tells the threshold that triggers a rescale of the axis relatively to the value.
+        const char* label;
+
+        LinePlotAxisGraphicsData()
+        {
+
+        };
+
+        LinePlotAxisGraphicsData(const char* _label) :
+            label(_label)
+        {
+            lowTolerance = 0.5f;
+            upTolerance = 0.5f;
+            axisRescaleLowThreshold = 0.1f;
+            axisRescaleUpThreshold = 0.9f;
+        };
+
+        inline float getLowLimit()
+        {
+            return low;
+        }
+
+        inline float getLowThresholdedValue()
+        {
+            return (up - low) * axisRescaleLowThreshold + low;
+        }
+
+        inline float getUpLimit()
+        {
+            return up;
+        }
+
+        inline float getUpThresholdedValue()
+        {
+            return (up - low) * axisRescaleUpThreshold + low;
+        }
+
+        inline void setLowLimit(float const& _x)
+        {
+            low = (1-lowTolerance) * _x;
+        }
+        
+        inline void setUpLimit(float const& _x)
+        {
+            up = (1+upTolerance) * _x;
+        }
+
+        inline bool isAboveUpThreshold(float const& _x)
+        {
+            return (_x > (up - low) * axisRescaleUpThreshold + low);
+        }
+
+        inline bool isBelowLowThreshold(float const& _x)
+        {
+            return (_x < (up - low)* axisRescaleLowThreshold + low);
+        }
+    };
+
+    struct LinePlotTabData : public TabData
+    {
+    public:
+        float x, y;
+        bool timeOnX, timeOnY;
+        int dataIdxToPlotOnX, dataIdxToPlotOnY;
+        LinePlotAxisGraphicsData xAxisGraphicsData;
+        LinePlotAxisGraphicsData yAxisGraphicsData;
+        
+        ScrollingBuffer buffer;
+
+        LinePlotTabData(short _id) :
+            TabData(_id), xAxisGraphicsData("X Axis"), yAxisGraphicsData("Y Axis")
+        {
+            sprintf(name, "Line Plot %d", _id);
+            x = 0.f;
+            y = 0.f;
+            timeOnX = false;
+            timeOnY = false;
+            dataIdxToPlotOnX = 0;
+            dataIdxToPlotOnY = 0;
+        }
+
+        inline float getXAxisLowLimit()
+        {
+            return xAxisGraphicsData.getLowLimit();
+        }
+
+        inline float getXAxisUpLimit()
+        {
+            return xAxisGraphicsData.getUpLimit();
+        }
+
+        inline float getYAxisLowLimit()
+        {
+            return yAxisGraphicsData.getLowLimit();
+        }
+
+        inline float getYAxisUpLimit()
+        {
+            return yAxisGraphicsData.getUpLimit();
+        }
+
+        inline void setScaleXAxis()
+        {
+            xAxisGraphicsData.setLowLimit(x);
+            xAxisGraphicsData.setUpLimit(x);
+        }
+
+        inline void setScaleYAxis()
+        {
+            yAxisGraphicsData.setLowLimit(y);
+            yAxisGraphicsData.setUpLimit(y);
+        }
+
+        inline void bufferAddPoint()
+        {
+            buffer.AddPoint(x, y);
+        }
+        
+        bool rescaleXAxis()
+        {
+            if (xAxisGraphicsData.isAboveUpThreshold(x) || xAxisGraphicsData.isBelowLowThreshold(x))
+            {
+                setScaleXAxis();
+                return true;
+            }
+            return false;
+        }
+
+        bool rescaleYAxis()
+        {
+            if (yAxisGraphicsData.isAboveUpThreshold(y) || yAxisGraphicsData.isBelowLowThreshold(y))
+            {
+                setScaleYAxis();
+                return true;
+            }
+            return false;
+        }
+    };
+
 	class ModelStateVisualizationWidget : public Widget
 	{
 	private:
+        char widgetNameID[32];
 		SimulationLoop* simulationLoop;//a pointer to the member of the same name in the Engine class.
-		const std::vector<std::string> displayCommandArray = { "display" };
+
+        const char* visualizationOptions[3] = { "Line Plot", "Quantity Table" };
+        std::vector<LinePlotTabData> lpTabsData;
+        std::vector<TabData> qtTabsData;
 
 		int nbSpecies;
 		ASTEvaluator* astEvaluator;
-		int dataIdxToPlotOnX = -1;
-		int dataIdxToPlotOnY = -1;
+        std::vector<std::string> speciesNamesBuffer;
 
-		void drawLinePlot();
+		void drawLinePlotTab(LinePlotTabData* _lpTabData);
 
-		void drawRawString();
-
-		void drawTable();
+		void drawQuantityTableTab(TabData* _tabData);
 
 	public:
-		ModelStateVisualizationWidget(CommandsManager* _cmdsManager, SimulationLoop* _simulationLoop) :
+		ModelStateVisualizationWidget(CommandsManager* _cmdsManager, SimulationLoop* _simulationLoop, short _id) :
 			Widget(_cmdsManager), simulationLoop(_simulationLoop)
 		{
-
+			astEvaluator = nullptr;
+			nbSpecies = 0;
+            sprintf(widgetNameID, "Model State Visualization##%d", _id);
 		}
 
 		void draw() override;
--- Editor/src/ModelStateVisualizationWidget.cpp
+++ Editor/src/ModelStateVisualizationWidget.cpp
@@ -1,213 +1,275 @@
 #include "ModelStateVisualizationWidget.hpp"
 
-// utility structure for realtime plot
-struct RollingBuffer
-{
-    float Span;
-    ImVector<ImVec2> Data;
-    RollingBuffer()
-    {
-        Span = 10.0f;
-        Data.reserve(2000);
-    }
-    void AddPoint(float x, float y)
-    {
-        float xmod = fmodf(x, Span);
-        if (!Data.empty() && xmod < Data.back().x)
-            Data.shrink(0);
-        Data.push_back(ImVec2(xmod, y));
-    }
-};
-
-// utility structure for realtime plot
-struct ScrollingBuffer
-{
-    int MaxSize;
-    int Offset;
-    ImVector<ImVec2> Data;
-    ScrollingBuffer(int max_size = 2000)
-    {
-        MaxSize = max_size;
-        Offset = 0;
-        Data.reserve(MaxSize);
-    }
-    void AddPoint(float x, float y)
-    {
-        if (Data.size() < MaxSize)
-            Data.push_back(ImVec2(x, y));
-        else
-        {
-            Data[Offset] = ImVec2(x, y);
-            Offset = (Offset + 1) % MaxSize;
-        }
-    }
-    void Erase()
-    {
-        if (Data.size() > 0)
-        {
-            Data.shrink(0);
-            Offset = 0;
-        }
-    }
-    void Reset()
-    {
-        Data.shrink(0);
-        Offset = 0;
-        Data.reserve(MaxSize);
-    }
-};
-
 void ECellEngine::Editor::ModelStateVisualizationWidget::draw()
 {
     ImGui::SetNextWindowSize(ImVec2(600, 300), ImGuiCond_FirstUseEver);
 
-    ImGui::Begin("Model State Visualization");
+    ImGui::Begin(widgetNameID);
+
+    //Draw Tab Generation Combo
+    static int visualOptionIdx = 0;
+    ImGui::Text("Visualization Tabs");
+    ImGui::SameLine();
+    if (ImGui::BeginCombo("##VisualizationTabs", visualizationOptions[visualOptionIdx]))
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            const bool is_selected = (visualOptionIdx == i);
+            if (ImGui::Selectable(visualizationOptions[i], is_selected))
+            {
+                visualOptionIdx = i;
+            }
+
+            // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+            if (is_selected)
+            {
+                ImGui::SetItemDefaultFocus();
+            }
+        }
+        ImGui::EndCombo();
+    }
 
     if (simulationLoop->getSimulationEnvironment()->isInitialized())
     {
-        nbSpecies = simulationLoop->getSimulationEnvironment()->getQuantities()->size();
-        astEvaluator = simulationLoop->getSimulationEnvironment()->astEvaluator;
+        //We can start adding tabs only if a simulation environement was initialized.
+        ImGui::SameLine();
 
-        ImGui::TextColored(ImVec4(0, 1, 0, 1), "A simulation environment has been set.");
-        if (ImGui::BeginTabBar("Visualization"))
+        if (ImGui::Button("Add Tab"))
         {
-            if (ImGui::BeginTabItem("String"))
+            switch (visualOptionIdx)
             {
-                drawRawString();
-                ImGui::EndTabItem();
-            }
-
-            if (ImGui::BeginTabItem("Table"))
-            {
-                drawTable();
-                ImGui::EndTabItem();
-            }
-
-            if (ImGui::BeginTabItem("LinePlot"))
-            {
-                drawLinePlot();
-                ImGui::EndTabItem();
+            case (0):
+                lpTabsData.push_back(LinePlotTabData((short)lpTabsData.size()));
+                break;
+            case (1):
+                qtTabsData.push_back(TabData((short)qtTabsData.size()));
+                break;
             }
         }
-        ImGui::EndTabBar();
+
+        //Whenever the number of species in the model state changes
+        if (nbSpecies != simulationLoop->getSimulationEnvironment()->getQuantities()->size())
+        {
+            nbSpecies = simulationLoop->getSimulationEnvironment()->getQuantities()->size();
+            astEvaluator = simulationLoop->getSimulationEnvironment()->astEvaluator;
+            
+            //Copy the names in the dedicated buffer
+            speciesNamesBuffer.clear();
+            speciesNamesBuffer.reserve(nbSpecies);
+            for (int i = 0; i < 2 * nbSpecies; i += 2)
+            {
+                speciesNamesBuffer.push_back(astEvaluator->getNode(i)->getNameEx());
+            }
+        }
+
+        ImGui::Separator();
     }
     else
     {
+        ImGui::Separator();
         ImGui::TextColored(ImVec4(1, 0, 0, 1), "No simulation environment has been set yet.");
+    }
+
+    static ImGuiTabBarFlags tabBarFlags = ImGuiTabBarFlags_Reorderable | ImGuiTabBarFlags_TabListPopupButton 
+        | ImGuiTabBarFlags_FittingPolicyScroll;
+    if (ImGui::BeginTabBar("##TabBar", tabBarFlags))
+    {
+        for(std::vector<LinePlotTabData>::iterator it = lpTabsData.begin(); it != lpTabsData.end(); it++)
+        {
+            drawLinePlotTab(&(*it));
+        }
+
+        for (std::vector<TabData>::iterator it = qtTabsData.begin(); it != qtTabsData.end(); it++)
+        {
+            drawQuantityTableTab(&(*it));
+        }
+        ImGui::EndTabBar();
     }
     
     ImGui::End();
 }
 
-void ECellEngine::Editor::ModelStateVisualizationWidget::drawLinePlot()
+void ECellEngine::Editor::ModelStateVisualizationWidget::drawLinePlotTab(LinePlotTabData* _lpTabData)
 {
-    static ImGuiTableFlags flags =
-         ImGuiTableFlags_Hideable | ImGuiTableFlags_Sortable
-        | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_ScrollY;
-
-    static float x, y;
-    static ScrollingBuffer   buffer;
-    static std::string labelX = "time (sec)";
-    static std::string labelY = "time (sec)";
-
-    ImGui::BulletText("This example assumes 60 FPS. Higher FPS requires larger buffer size.");
-    ImGui::Text("dataIdxToPlotOnX: %1d; dataIdxToPlotOnY: %1d;", dataIdxToPlotOnX, dataIdxToPlotOnY);
-    ImGui::Text("elapsedTime: %3f; deltaTime: %f;", simulationLoop->simulationTimer.elapsedTime, simulationLoop->simulationTimer.deltaTime);
-    ImGui::Text("x: %3f; y: %f;", x, y);
-
-    if (ImGui::BeginTable("Choose the data to plot against each other", 2, flags, ImVec2(150,300)))
+    if (ImGui::BeginTabItem(_lpTabData->name))
     {
-        ImGui::TableSetupColumn("Data to plot on X");
-        ImGui::TableSetupColumn("Data to plot on Y");
-        ImGui::TableHeadersRow();
-        ImGui::TableNextRow();
-        for (int i = 0; i < 2 * nbSpecies; i += 2)
+        ImGui::BulletText("This example assumes 60 FPS. Higher FPS requires larger buffer size.");
+        ImGui::Text("dataIdxToPlotOnX: %1d; dataIdxToPlotOnY: %1d;", _lpTabData->dataIdxToPlotOnX, _lpTabData->dataIdxToPlotOnY);
+        ImGui::Text("elapsedTime: %3f; deltaTime: %f;", simulationLoop->simulationTimer.elapsedTime, simulationLoop->simulationTimer.deltaTime);
+        ImGui::Text("x: %3f; y: %f;", _lpTabData->x, _lpTabData->y);
+
+        if (ImGui::CollapsingHeader("X axis"))
         {
-            ImGui::TableNextColumn();
-            /*if (ImGui::Button(astEvaluator->getNode(i)->getNameEx().c_str()))
+            ImGui::Checkbox("Plot time on X", &_lpTabData->timeOnX);
+            
+            if (_lpTabData->timeOnX)
             {
-                dataIdxToPlotOnX = i;
-            }*/
-            ImGui::Text(astEvaluator->getNode(i)->getNameEx().c_str());
-            ImGui::TableNextColumn();
-            if (ImGui::Button(astEvaluator->getNode(i)->getNameEx().c_str()))
-            {
-                dataIdxToPlotOnY = i;
-                buffer.Reset();
+                _lpTabData->xAxisGraphicsData.label = "Time (sec)";
+
+                //Disable the Combo to chose species if we decide to map time
+                ImGui::BeginDisabled();
             }
-            ImGui::TableNextRow();
+
+            // Combos to chose what species to plot on X
+            if (ImGui::BeginCombo("Species to map on X axis", speciesNamesBuffer[_lpTabData->dataIdxToPlotOnX].c_str()))
+            {
+                for (int i = 0; i < nbSpecies; i++)
+                {
+                    const bool is_selected = (_lpTabData->dataIdxToPlotOnX == i);
+                    if (ImGui::Selectable(speciesNamesBuffer[i].c_str(), is_selected))
+                    {
+                        _lpTabData->dataIdxToPlotOnX = i;
+                        _lpTabData->xAxisGraphicsData.label = speciesNamesBuffer[i].c_str();
+                        _lpTabData->buffer.Reset();
+                    }
+
+                    // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                    if (is_selected)
+                    {
+                        ImGui::SetItemDefaultFocus();
+                    }
+                }
+                ImGui::EndCombo();
+            }
+
+            if (_lpTabData->timeOnX)
+            {
+                ImGui::EndDisabled();
+            }
+
+            if (ImGui::TreeNode("Parameters##X"))
+            {
+                ImGui::SliderFloat("Low tolerance##X", &_lpTabData->xAxisGraphicsData.lowTolerance, 0.f, 1.f);
+                ImGui::SliderFloat("Up tolerance##X", &_lpTabData->xAxisGraphicsData.upTolerance, 0.f, 1.f);
+                ImGui::SliderFloat("Low rescale threshold##X", &_lpTabData->xAxisGraphicsData.axisRescaleLowThreshold, 0.f, _lpTabData->xAxisGraphicsData.axisRescaleUpThreshold - 0.001f);
+                ImGui::SliderFloat("Up rescale threshold##X", &_lpTabData->xAxisGraphicsData.axisRescaleUpThreshold, _lpTabData->xAxisGraphicsData.axisRescaleLowThreshold + 0.001f, 1.f);
+
+                ImGui::TreePop();
+            }
         }
-        ImGui::EndTable();
-    }
 
-    ImGui::SameLine();
+        ImGui::Spacing();
 
-    
-    x = simulationLoop->simulationTimer.elapsedTime;
-    if (dataIdxToPlotOnX > -1)
-    {
-        labelX = astEvaluator->getNode(dataIdxToPlotOnX)->getNameEx();
-        x = astEvaluator->getNamedNodeValue(dataIdxToPlotOnX);
-    }
-
-    y = simulationLoop->simulationTimer.elapsedTime;
-    if (dataIdxToPlotOnY > -1)
-    {
-        labelY = astEvaluator->getNode(dataIdxToPlotOnY)->getNameEx();
-        y = astEvaluator->getNamedNodeValue(dataIdxToPlotOnY);
-    }
-
-    buffer.AddPoint(x, y);
-
-    static float history = 10.0f;
-    //buffer.Span = 10.0f;
-
-    if (ImPlot::BeginPlot("Plot", ImVec2(530, 300), ImPlotFlags_NoLegend))
-    {
-        ImPlot::SetupAxes(labelX.c_str(), labelY.c_str());
-        //ImPlot::SetupAxisLimits(ImAxis_X1, 0, history, ImGuiCond_Always);
-        ImPlot::SetupAxisLimits(ImAxis_X1,x - history, x, ImGuiCond_Always);
-
-        //ImPlot::SetupAxisLimits(ImAxis_Y1, 0, history, ImGuiCond_Always);
-        ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 100000);
-
-        ImPlot::PlotLine("data", &buffer.Data[0].x, &buffer.Data[0].y, buffer.Data.size(), 0, 2 * sizeof(float));
-        ImPlot::EndPlot();
-    }
-}
-
-void ECellEngine::Editor::ModelStateVisualizationWidget::drawRawString()
-{
-    ImGui::Text("Legacy Raw string format");
-    if (ImGui::Button("Print"))
-    {
-        engineCmdsManager->interpretCommand(displayCommandArray);
-    }
-}
-
-
-void ECellEngine::Editor::ModelStateVisualizationWidget::drawTable()
-{
-    static ImGuiTableFlags flags =
-        ImGuiTableFlags_Resizable | ImGuiTableFlags_Hideable | ImGuiTableFlags_Sortable
-        | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_ScrollY;
-
-    ImGui::Text("The model state has %1d species", nbSpecies);
-    if (ImGui::BeginTable("Loaded SBML Documents", 2, flags))
-    {
-        ImGui::TableSetupColumn("Species Id");
-        ImGui::TableSetupColumn("Quantity");
-        ImGui::TableHeadersRow();
-        ImGui::TableNextRow();
-        for (int i = 0; i < 2 * nbSpecies; i += 2)
+        if (ImGui::CollapsingHeader("Y axis"))
         {
-            ImGui::TableNextColumn();
-            ImGui::Text(astEvaluator->getNode(i)->getNameEx().c_str());
-            ImGui::TableNextColumn();
-            ImGui::Text("%2f", astEvaluator->getNamedNodeValue(i));
-            ImGui::TableNextRow();
+            ImGui::Checkbox("Plot time on Y", &_lpTabData->timeOnY);
+
+            if (_lpTabData->timeOnY)
+            {
+                _lpTabData->yAxisGraphicsData.label = "Time (sec)";
+
+                //Disable the Combo to chose species if we decide to map time
+                ImGui::BeginDisabled();
+            }
+
+            // Combos to chose what species to plot on Y
+            if (ImGui::BeginCombo("Species to map on Y axis", speciesNamesBuffer[_lpTabData->dataIdxToPlotOnY].c_str()))
+            {
+                for (int i = 0; i < nbSpecies; i++)
+                {
+                    const bool is_selected = (_lpTabData->dataIdxToPlotOnY == i);
+                    if (ImGui::Selectable(speciesNamesBuffer[i].c_str(), is_selected))
+                    {
+                        _lpTabData->dataIdxToPlotOnY = i;
+                        _lpTabData->yAxisGraphicsData.label = speciesNamesBuffer[i].c_str();
+                        _lpTabData->buffer.Reset();
+                    }
+
+                    // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                    if (is_selected)
+                    {
+                        ImGui::SetItemDefaultFocus();
+                    }
+                }
+                ImGui::EndCombo();
+            }
+
+            if (_lpTabData->timeOnY)
+            {
+                ImGui::EndDisabled();
+            }
+
+            if (ImGui::TreeNode("Parameters##Y"))
+            {
+                ImGui::SliderFloat("Low tolerance##Y", &_lpTabData->yAxisGraphicsData.lowTolerance, 0.f, 1.f);
+                ImGui::SliderFloat("Up tolerance##Y", &_lpTabData->yAxisGraphicsData.upTolerance, 0.f, 1.f);
+                ImGui::SliderFloat("Low rescale threshold##Y", &_lpTabData->yAxisGraphicsData.axisRescaleLowThreshold, 0.f, _lpTabData->yAxisGraphicsData.axisRescaleUpThreshold - 0.001f);
+                ImGui::SliderFloat("Up rescale threshold##Y", &_lpTabData->yAxisGraphicsData.axisRescaleUpThreshold, _lpTabData->yAxisGraphicsData.axisRescaleLowThreshold + 0.001f, 1.f);
+
+                ImGui::TreePop();
+            }
+        }        
+
+        _lpTabData->x = simulationLoop->simulationTimer.elapsedTime;
+        if (!_lpTabData->timeOnX)
+        {
+            _lpTabData->x = astEvaluator->getNamedNodeValue(2*_lpTabData->dataIdxToPlotOnX);
         }
 
-        ImGui::EndTable();
+        _lpTabData->y = simulationLoop->simulationTimer.elapsedTime;
+        if (!_lpTabData->timeOnY)
+        {
+            _lpTabData->y = astEvaluator->getNamedNodeValue(2*_lpTabData->dataIdxToPlotOnY);
+        }
+
+        _lpTabData->bufferAddPoint();
+
+        if (ImPlot::BeginPlot("Plot", ImVec2(530, 530), ImPlotFlags_NoLegend))
+        {
+            ImPlot::SetupAxes(_lpTabData->xAxisGraphicsData.label, _lpTabData->yAxisGraphicsData.label);
+
+            if (_lpTabData->rescaleXAxis())
+            {
+                ImPlot::SetupAxisLimits(ImAxis_X1,
+                    _lpTabData->getXAxisLowLimit(),
+                    _lpTabData->getXAxisUpLimit(), ImGuiCond_Always);
+            }
+
+            if (_lpTabData->rescaleYAxis())
+            {
+                ImPlot::SetupAxisLimits(ImAxis_Y1,
+                    _lpTabData->getYAxisLowLimit(),
+                    _lpTabData->getYAxisUpLimit(), ImGuiCond_Always);
+            }       
+
+            ImPlot::TagX(_lpTabData->xAxisGraphicsData.getLowThresholdedValue(), ImVec4(1, 1, 0, 1));
+            ImPlot::TagX(_lpTabData->xAxisGraphicsData.getUpThresholdedValue(), ImVec4(1, 1, 0, 1));
+            ImPlot::TagY(_lpTabData->yAxisGraphicsData.getLowThresholdedValue(), ImVec4(1, 1, 0, 1));
+            ImPlot::TagY(_lpTabData->yAxisGraphicsData.getUpThresholdedValue(), ImVec4(1, 1, 0, 1));
+
+            ImPlot::PlotLine("data", &_lpTabData->buffer.Data[0].x, &_lpTabData->buffer.Data[0].y, _lpTabData->buffer.Data.size(), 0, 2 * sizeof(float));
+            ImPlot::EndPlot();
+        }
+        ImGui::EndTabItem();
+    }
+}
+
+
+void ECellEngine::Editor::ModelStateVisualizationWidget::drawQuantityTableTab(TabData* _tabData)
+{
+    if (ImGui::BeginTabItem(_tabData->name))
+    {
+        const ImGuiTableFlags flags =
+            ImGuiTableFlags_Resizable | ImGuiTableFlags_Hideable | ImGuiTableFlags_Sortable
+            | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_ScrollY;
+
+        if (ImGui::BeginTable("Loaded SBML Documents", 2, flags))
+        {
+            ImGui::TableSetupColumn("Species Id");
+            ImGui::TableSetupColumn("Quantity");
+            ImGui::TableHeadersRow();
+            ImGui::TableNextRow();
+            for (int i = 0; i < nbSpecies; i++)
+            {
+                ImGui::TableNextColumn();
+                ImGui::Text(speciesNamesBuffer[i].c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text("%2f", astEvaluator->getNamedNodeValue(2 * i));
+                ImGui::TableNextRow();
+            }
+
+            ImGui::EndTable();
+        }
+        ImGui::EndTabItem();
     }
 }
