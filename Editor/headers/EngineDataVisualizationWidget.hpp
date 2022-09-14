cb241f41e93223b11a7e0648ae18fde7336f1858
Eliott <eliott.jacopin@laposte.net>
2022-09-14 14:31:00 +0900

    [Update] Documentation on EngineDataVisualizationWidget

--- Editor/headers/EngineDataVisualizationWidget.hpp
+++ Editor/headers/EngineDataVisualizationWidget.hpp
@@ -14,15 +14,23 @@
 
 namespace ECellEngine::Editor
 {
+	/*
+	@brief The widget from which every other widget to visualize
+			data about the engine will be instanced and stored.
+	*/
 	class EngineDataVisualizationWidget : public Widget
 	{
 	private:
 		CommandsManager* cmdsManager;
 		SimulationLoop* simuLoop;
 
-		short nbMsvWidgets;
-		std::vector<std::unique_ptr<ModelStateVisualizationWidget>> msvWidgets;
+		short nbMsvWidgets;//stored to pass on the newly instanciated msvWidget as their ID.
+		std::vector<std::unique_ptr<ModelStateVisualizationWidget>> msvWidgets;//the list of instantiated msvWidgets
 
+		/*
+		@brief Draw all the instanciated widgets stored in the
+			   different relevant vectors.
+		*/
 		void drawInstancedWidgets();
 
 	public:
