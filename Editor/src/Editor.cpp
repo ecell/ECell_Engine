69d9ace8ab75541912c77522502cb0d504ce71b9
Jacopin Eliott <eliott.jacopin@laposte.net>
2023-02-27 17:48:09 +0900

    [Add] ImGui Docking
    Major:
    - Switch to ImGui docking branch
    - enabled docking as default

--- Editor/src/Editor.cpp
+++ Editor/src/Editor.cpp
@@ -148,6 +148,7 @@ void ECellEngine::Editor::Editor::initializeEditorWindow()
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
