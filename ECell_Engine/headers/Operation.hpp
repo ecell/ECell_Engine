a3204d246b2dd6770cf89ae6a29229f9404a255c
Jacopin Eliott <eliott.jacopin@laposte.net>
2023-03-20 17:00:20 +0100

    [Fix] SBML ASTNode parsing
    Major:
    - Many pointers between operations to make the complete tree of a mathematical equation were broken with the previous version
    - We wrote  a new algorithm to parse libSBML ASTNodes. The new version is not recursive but relies on a stack.
    - Documentation was also added

--- ECell_Engine/headers/Operation.hpp
+++ ECell_Engine/headers/Operation.hpp
@@ -3,38 +3,182 @@
 #include <memory>
 #include <vector>
 
-#include "Constant.hpp"
+#include "Constant.hpp" //#include "Operand.hpp"
 #include "Function.hpp"
 
 namespace ECellEngine::Maths
 {
+    /*!
+    @brief The data struct to describe a mathematical operation.
+    @details It is designed for binary operations. A, operation may have other
+             operations as its operands.
+    */
     struct Operation : public Operand
     {
     private:
+        /*!
+        @brief The function to perform corresponding to the Operation.
+        */
         const Function* function;
+
+        /*!
+        @brief A byte used to encode how the operation was built.
+        @details It is typically usefull if the operation uses other operations
+                 or numeric constants as its operands. Indeed, as constants and
+                 operations are stored locally in ::constants and ::operations
+                 (as opposed to global variables that are stored in the datastate),
+                 we need to ascertain which of the two is the left and right operand.
+                 In the parsing process, we assume that left operands are added first
+                 and the right operands are added last. Therefore, we use 5 bits out
+                 of the 8 to describe if one or two local operands are used and which
+                 one was it in which order. The relevant bits are grouped as: 765 4 32 10.
+                 We do not use the 5th, 6th and 7th bits.
+                 
+                 Bits 0 and 1 are used to indicate whether one (Bit 0 = 1) or
+                 two (Bit 0 = 1 & Bit 1 = 1) local operands are used.
+                 
+                 Bit 2 indicates whether the 1st local operand is a Constant
+                 (Bit 2 = 0) or another operation (Bit 2 = 0).
+
+                 Bit 3 indicates whether the 1st local operand is the left operand
+                 (Bit 3 = 0) or the right operand (Bit 3 = 1). This bit is important
+                 to detect if a global variable from the datastate has already
+                 been added as a left operand to the operation.
+                 
+                 Finally, Bit 4 is equivalent to Bit 2 (distinguish between local
+                 constant or another operation) but specifically when a local
+                 operand has already been added as the left operand.
+
+        @see The source code of ::AddConstant and ::AddOperation for a demonstration
+             of how this byte is written.
+        @see The source code of ::PushOperands for a demonstration of how this byte
+             is read.
+        */
+        unsigned char structure;
+
+        /*!
+        @brief The list of local constants used in the operation.
+        */
         std::vector<Constant> constants;
+
+        /*!
+        @brief The list of local operations used in this operation.
+        @details This is equivalent to children operations which builds a tree
+                 when taken all together.
+        */
+        std::vector<Operation> operations;
+
+        /*!
+        @brief The list of right and left operands used in this operation.
+        @details It may contain pointers to elements in ::constants, ::operations
+                 or another variable in the datastate.
+        */
         std::vector<Operand*> operands;
 
-    public:
-        Operation() = default;
+        /*!
+        @brief Decodes ::structure to know in which order operands stored
+               in ::constants or ::operations must be added in ::operands.
+        */
+        void PushOperands();
 
+        /*!
+        @brief Shrinks the capacity of the vectors ::constants and
+                ::operations.
+        @details This is relevant to save space once we know exactly
+                 how many constants or other operations are used as
+                 operands of this operation.
+        */
+        inline void ShrinkLocalOperands() noexcept
+        {
+            constants.shrink_to_fit();
+            operations.shrink_to_fit();
+        }
+
+    public:
         Operation(const std::string _name) : 
             Operand (_name)
         {
-
+            function = nullptr;
+            structure = 0;
+            constants.reserve(2);
+            operations.reserve(2);
+            operands.reserve(2);
         }
 
-        inline void AddConstant(float _value)
+        /*!
+        @brief Adds a constant in ::constants from its @p _value
+        @details Also writes in ::structure to encode this new operand's
+                 information.
+        */
+        inline Constant& AddConstant(const float _value)
         {
             constants.push_back(Constant(_value));
-            operands.push_back(&constants.back());
+
+            if (structure == 0)// 000 0 00 00
+            {
+                structure |= 1 << 0;// 000 0 00 01
+                if (operands.size() > 0)
+                {
+                    structure |= 1 << 3;// 000 0 10 01
+                }
+            }
+            else
+            {
+                structure |= 1 << 1;// 000 0 XX 11
+            }
+
+            return constants.back();
         }
 
+        /*!
+        @brief Adds an operand in ::operands.
+        @details Mostly to use when adding a global variable stored in the 
+                 datastate as an operand in this operation.
+        */
         inline void AddOperand(Operand* _operand)
         {
             operands.push_back(_operand);
         }
 
+        /*!
+        @brief Adds an operation in ::operations from its @p _name
+        @details Also writes in ::structure to encode this new operand's
+                 information.
+        */
+        inline Operation& AddOperation(const std::string _name)
+        {
+            operations.push_back(Operation(_name));
+
+            if (structure == 0)// 000 0 00 00
+            {
+                structure |= 1 << 0;// 000 0 00 01 --> We add the first operand
+                structure |= 1 << 2;// 000 0 01 01 --> The first operand is an Operation
+                
+                //if there is already one element in operands,
+                //we register the operation as the second operand.
+                // --> The bit 4 (so idx 3) is set to 1
+                if (operands.size() > 0)
+                {
+                    structure |= 1 << 3;// 000 0 11 01
+                }
+            }
+            else
+            {
+                structure |= 1 << 1;// 000 0 XX 11 --> We add the second operand
+                structure |= 1 << 4;// 000 1 XX 11 --> The second operand is an Operation
+            }
+
+            return operations.back();
+        }
+
+        /*!
+        @brief Retrieves and fills the list @p _operandsNames with the names of
+               all the operands' names of this operation (and the whole tree).
+        @paramt OperandType A specific type of operand to chose from. It MUST
+                derivates from ECellEngine::Maths::Operand.
+        @param _operandsNames The reference to the vector where to store the
+                names of the target operands.
+        */
         template<typename OperandType>//, typename = std::enable_if_t<std::is_base_of_v<Operand, OperandType>>>
         void GetOperandsNames(std::vector<std::string>& _operandsNames) const noexcept
         {
@@ -44,23 +188,52 @@ namespace ECellEngine::Maths
                 {
                     _operandsNames.push_back((*it)->name);
                 }
+            }
 
-                Operation* op = dynamic_cast<Operation*>(*it);
-                if (op != nullptr)
-                {
-                    op->GetOperandsNames<OperandType>(_operandsNames);
-                }
+            for (std::vector<Operation>::const_iterator it = operations.begin(); it != operations.end(); it++)
+            {
+                it->GetOperandsNames<OperandType>(_operandsNames);
             }
         }
 
+        /*!
+        @brief Gets the result of this operation.
+        @details Uses ::function and ::operands to compute the operation.
+        */
         inline virtual float Get() const noexcept override
         {
             return (*function)(operands);
         }
 
+        /*!
+        @brief Checks if there is at least two operands stored in this operation.
+        @returns True if the sum of the size of ::operands, ::operations and
+                 ::constants is greater or equal to 2.
+        */
+        inline bool IsFull()
+        {
+            return (operands.size() + operations.size() + constants.size() >= 2);
+        }
+
+        /*!
+        @brief Recursively pushes operands in ::operands and trims unsused
+                memory space for every child operation of this operation.
+        @details Uses ::PushOperands and ::ShrinkLocalOperands.
+        */
+        void LinkLocalOperands();
+
+        /*!
+        @brief Sets the ::function that will be called when computing this
+                operation.
+        @param _function The pointer to the relevant mathematical function
+                to link this operation.
+        @see ECellEngine::Maths::Function.
+        */
         inline void Set(const Function* _function) noexcept
         {
             function = _function;
         }
+
+        
     };
 }
--- ECell_Engine/headers/SBMLModuleImporter.hpp
+++ ECell_Engine/headers/SBMLModuleImporter.hpp
@@ -1,31 +1,127 @@
 #pragma once 
 
-#include <filesystem>
+#include <filesystem> //for path
 #include <unordered_map> //for unordered_multimap
 
 #include <sbml/SBMLTypes.h>
 
-#include "Constant.hpp"
 #include "Function.hpp"
 #include "ModuleImporter.hpp"
 #include "SBMLModule.hpp"
 
 namespace ECellEngine::IO
 {
+	/*!
+	@brief The importer specialized for SBML files.
+	*/
 	class SBMLModuleImporter : public ModuleImporter
 	{
 	private:
-		const bool ValidateSBML(SBMLDocument* _sbmlDocument);
 
+		/*!
+		@brief Translates an ASTNode (and the tree below) from libSBML into
+				its representation in the engine (an ECellEngine::Maths::Operation).
+		@details Works with a heap to go through the ASTNode tree and build our own
+				 within a root operation that is returned.
+		@param _rootAstNode The root from which we translate the equation.
+		@param _rootName The name we want to associate to the operation.
+		@param _dataState Reference to a datastate containing named variables that
+				may be used in the operation.
+		@param _docIdsToDataStateNames Reference to a local unordered map between the
+				names used as IDs in SBML and the names we use in the data state.
+				They may be identical. This object is instantiated in ::TryImport and
+				survives only during import.
+		@returns The root ECellEngine::Maths::Operation that represents the equation.
+		*/
+		Operation ASTNodeToOperation(
+			const ASTNode* _rootAstNode, const std::string _rootName,
+			ECellEngine::Data::DataState& _dataState,
+			std::unordered_map<std::string, std::string>& _docIdsToDataStateNames);
+
+		/*!
+		@brief Assigns a ECellEngine::Maths::Function to @p _op depending on the
+				type of operation of @p _nope.
+		@remarks It uses a switch statement with no default. So, in the event that
+				 the type of @p _node is not described, the function member of @p _op
+				 will stay equal to @a nullptr
+		*/
+		void AssignOperationFunction(Operation& _op, const ASTNode* _node);
+
+		/*!
+		@brief Parses the list of parameters encoded in the sbml file.
+		*/
 		void InitializeParameters(ECellEngine::Data::DataState& _dataState, ECellEngine::Data::SBMLModule& _sbmlModule, const Model* _model, std::unordered_map<std::string, std::string>& _docIdsToDataStateNames);
 
+		/*!
+		@brief Parses the list of reactions encoded in the sbml file.
+		*/
 		void InitializeReactions(ECellEngine::Data::DataState& _dataState, ECellEngine::Data::SBMLModule& _sbmlModule, const Model* _model, std::unordered_map<std::string, std::string>& _docIdsToDataStateNames);
 
+		/*!
+		@brief Parses the list of species encoded in the sbml file.
+		*/
 		void InitializeSpecies(ECellEngine::Data::DataState& _dataState, ECellEngine::Data::SBMLModule& _sbmlModule, const Model* _model, std::unordered_map<std::string, std::string>& _docIdsToDataStateNames);
 
-		Operation* ASTNodeToOperation(ECellEngine::Data::DataState& _dataState, const std::string& _rootName, const ASTNode* _node, const std::unordered_map<std::string, std::string>& _docIdsToDataStateNames);
+		/*!
+		@brief Tests if the ASTNode has a type that is considered as an operation.
+		@returns Returns true for:  ASTNodeType_t::AST_PLUS, ASTNodeType_t::AST_MINUS,
+				 ASTNodeType_t::AST_TIMES, ASTNodeType_t::AST_DIVIDE, ASTNodeType_t::AST_FUNCTION_POWER.
+				 False, otherwise.
+		*/
+		bool IsASTNodeOperation(const ASTNode* _node);
+
+		/*!
+		@brief Performs a suite of checks on the sbml document to insure its integrity.
+		@details Validates and pretty-prints the warnings or errors if any.
+		@remark Adapted from: example.cpp (https://sbml.org/software/libsbml/5.18.0/docs/formatted/cpp-api/libsbml-example.html).
+		@remark This sample program is distributed under a different license than the rest
+				 of libSBML.  This program uses the open-source MIT license, as follows:
+				 
+				 -----------------------------------------------------------------------
+				 
+				 Copyright (c) 2013-2018 by the California Institute of Technology
+				 (California, USA), the European Bioinformatics Institute (EMBL-EBI, UK)
+				 and the University of Heidelberg (Germany), with support from the National
+				 Institutes of Health (USA) under grant R01GM070923.  All rights reserved.
+				 
+				 Permission is hereby granted, free of charge, to any person obtaining a
+				 copy of this software and associated documentation files (the "Software"),
+				 to deal in the Software without restriction, including without limitation
+				 the rights to use, copy, modify, merge, publish, distribute, sublicense,
+				 and/or sell copies of the Software, and to permit persons to whom the
+				 Software is furnished to do so, subject to the following conditions:
+				 
+				 The above copyright notice and this permission notice shall be included in
+				 all copies or substantial portions of the Software.
+				 
+				 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+				 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+				 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+				 THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+				 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+				 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+				 DEALINGS IN THE SOFTWARE.
+				 
+				 Neither the name of the California Institute of Technology (Caltech), nor
+				 of the European Bioinformatics Institute (EMBL-EBI), nor of the University
+				 of Heidelberg, nor the names of any contributors, may be used to endorse
+				 or promote products derived from this software without specific prior
+				 written permission.
+
+				 -----------------------------------------------------------------------
+
+		@author  Akiya Jouraku
+		*/
+		const bool ValidateSBML(SBMLDocument* _sbmlDocument);
 
 	public:
+		/*!
+		@brief Tries to parse a file.
+		@param _filePath The system's path to the file.
+		@param _dataState Reference to a datastate where additional data will be stored
+				and containing other named variables that may be used during the import process.
+		@returns If successful, an asset object encapsulated in a shared pointer. @a nullptr, otherwise.
+		*/
 		virtual const std::shared_ptr<ECellEngine::Data::Module> TryImport(const std::filesystem::path& _filePath, ECellEngine::Data::DataState& _dataState) noexcept override;
 	};
 }
