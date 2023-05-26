7f84a75512b8cade7a15238a204f8710d46d65c3
Jacopin Eliott <eliott.jacopin@laposte.net>
2023-05-26 17:34:22 +0900

    [Fix] Definition of Parameter::GetInvolvdeParameters(...)
    Had forgotten to write it

--- ECell_Engine/src/Data/Parameter.cpp
+++ ECell_Engine/src/Data/Parameter.cpp
@@ -1,1 +1,8 @@
 #include "Data/Parameter.hpp"
+
+void ECellEngine::Data::Parameter::GetInvolvedParameters(std::vector<std::string>& out_involvedParameters, bool clearOutVector) const noexcept
+{
+	Operand::GetInvolvedParameters(out_involvedParameters, clearOutVector);
+
+	out_involvedParameters.emplace_back(name);
+}
