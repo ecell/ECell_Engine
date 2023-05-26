ce1a9efbea2d0f05ff317596f9db6ae695386893
Jacopin Eliott <eliott.jacopin@laposte.net>
2023-05-26 15:53:41 +0900

    [Delete] SBMLModule
    Major:
    - The only difference between an SBML Module and a Biochemical module was the list of computed parameters.
    - Now, it doesn't really make sense to differentiate SBMLModules with BioChemicalModules on the basis of equations (external to reaction kinetic laws)
    - Another major argument is that equations will need to be updated at various timings various (including inside solvers' steps). But, solvers are related to biochemical modules. So, it would mean further adapting solvers for each type of files (e.g. sbml) instead of types of system (biochemical)

--- ECell_Engine/headers/Data/BiochemicalModule.hpp
+++ ECell_Engine/headers/Data/BiochemicalModule.hpp
@@ -10,8 +10,9 @@ namespace ECellEngine::Data
 	class BiochemicalModule : public Module
 	{
 	protected:
+		std::vector<std::string> equations;
 		std::vector<std::string> reactions;
-		std::vector<std::string> simpleParameters;
+		std::vector<std::string> parameters;
 		std::vector<std::string> species;
 
 	public:
@@ -21,19 +22,29 @@ namespace ECellEngine::Data
 
 		}
 
+		inline const std::vector<std::string>& GetAllEquation() const noexcept
+		{
+			return equations;
+		}
+
 		inline const std::vector<std::string>& GetAllReaction() const noexcept
 		{
 			return reactions;
 		}
 
-		inline const std::vector<std::string>& GetAllSimpleParameter () const noexcept
+		inline const std::vector<std::string>& GetAllParameter () const noexcept
 		{
-			return simpleParameters;
+			return parameters;
 		}
 
 		inline const std::vector<std::string>& GetAllSpecies() const noexcept
 		{
 			return species;
+		}
+		
+		inline const std::string& GetEquation(const std::size_t& _idx) const noexcept
+		{
+			return equations[_idx];
 		}
 
 		inline const std::string& GetReaction(const std::size_t& _idx) const noexcept
@@ -41,14 +52,22 @@ namespace ECellEngine::Data
 			return reactions[_idx];
 		}
 
-		inline const std::string& GetSimpleParameter(const std::size_t& _idx) const noexcept
+		inline const std::string& GetParameter(const std::size_t& _idx) const noexcept
 		{
-			return simpleParameters[_idx];
+			return parameters[_idx];
 		}
 
 		inline const std::string& GetSpecies(const std::size_t& _idx) const noexcept
 		{
 			return species[_idx];
+		}
+
+		inline void AddEquation(const std::string _equationName, const Operation _parameterOp)
+		{
+			if (dataState.AddEquation(_equationName, _parameterOp))
+			{
+				equations.push_back(_equationName);
+			}
 		}
 
 		inline void AddReaction(const std::string _reactionName,
@@ -62,11 +81,11 @@ namespace ECellEngine::Data
 			}
 		}
 
-		inline void AddSimpleParameter(const std::string _parameterName, const float _value)
+		inline void AddParameter(const std::string _parameterName, const float _value)
 		{
-			if (dataState.AddSimpleParameter(_parameterName, _value))
+			if (dataState.AddParameter(_parameterName, _value))
 			{
-				simpleParameters.push_back(_parameterName);
+				parameters.push_back(_parameterName);
 			}
 		}
 
