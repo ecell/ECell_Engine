fd8614cfaf8d8dc3e6ac298a1817478207ddd9fc
Jacopin Eliott <eliott.jacopin@laposte.net>
2023-06-24 00:38:36 +0900

    [Add] Min and Max functions in Math Utility
    Major:
    - Added custom (copied from cpp reference suggested implementation) min and max functions to avoid having to include the whole algorithm header
    
    Minor:
    - Commented out unused code and removed unused cpp from the CMakeLists

--- ECell_Engine/headers/Maths/MathUtility.hpp
+++ ECell_Engine/headers/Maths/MathUtility.hpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <math.h>
 
 namespace ECellEngine::Maths
 {
@@ -6,6 +6,32 @@ namespace ECellEngine::Maths
 	/// Computes !_n
 	/// </summary>
 	int fact(int _n);
+
+	/*!
+	@brief Computes the maximum of two values.
+	@param a The first value.
+	@param b The second value.
+	@paramt T The type of the values.
+	@return The maximum of a and b.
+	*/
+	template<class T>
+	static inline const T& max(const T& a, const T& b)
+	{
+		return (a > b) ? a : b;
+	}
+
+	/*!
+	@brief Computes the minimum of two values.
+	@param a The first value.
+	@param b The second value.
+	@paramt T The type of the values.
+	@return The minimum of a and b.
+	*/
+	template<class T>
+	static inline const T& min(const T& a, const T& b)
+	{
+		return (a < b) ? a : b;
+	}
 
 	///<summary>
 	/// Computes the number of subsets of size _r made
@@ -23,7 +49,7 @@ namespace ECellEngine::Maths
 	/// <summary>
 	/// Writes the mathematical expression of !_n as a MathML string.
 	/// </summary>
-	std::string str_fact(int _n);
+	//std::string str_fact(int _n);
 
 	/// <summary>
 	/// Writes the mathematical expression of nCr as a MathML string.
@@ -32,11 +58,11 @@ namespace ECellEngine::Maths
 	/// total quantity of distinct elements as a silent vaiable.</param>
 	/// <param name="_r">An integer giving the size of the subset</param>
 	/// <returns>A string such as n*(n-1)*0.5</returns>
-	std::string str_nCR(std::string _n, int _r);
+	//std::string str_nCR(std::string _n, int _r);
 
 	/// <summary>
 	/// Writes the mathematical expression reversed factorial of _n
 	/// as a MathML string.
 	/// </summary>
-	std::string str_revFact(std::string _n, int _i);
+	//std::string str_revFact(std::string _n, int _i);
 }
